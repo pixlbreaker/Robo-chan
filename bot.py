95d952fad251d5541d68d22fc18aea729096785a
Mike Skotar <michaelskotar@gmail.com>
2019-10-31 00:11:12 -0400

    Something is broken

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import discord
 import asyncio
 import json
-import time
+import random
 import os
+import youtube_dl
 from urllib.request import Request, urlopen
 from discord.ext import commands
 from random import randint
@@ -12,6 +13,8 @@ f = open("info.json")
 data = json.load(f)
 TOKEN = data['TOKEN']
 bot = commands.Bot(command_prefix = ".")
+
+players = {}
 
 # Ready Event
 @bot.event
@@ -34,20 +37,38 @@ async def ping(ctx):
     await ctx.send(latency)
 
 @bot.command(pass_context=True)
+async def flip(ctx):
+    flip = random.choice(["Heads", "Tails"])
+    await bot.send_message(ctx.message.channel, flip)
+
+@bot.command(pass_context=True)
 async def join(ctx):
     '''
     Joins the users channel
     '''
-    channel = ctx.message.author.voice.voice_channel
-    await bot.join_voice_channel(channel)
+    guild = ctx.message.guild
+    await bot.join_voice_channel(guild)
+
+@bot.command(pass_context=True)
+async def play(ctx, url):
+    '''
+    Plays a current url
+    '''
+    guild = ctx.message.guild
+    voice_client = guild.voice_client
+    player = await voice_client.create_ytdl_player(url)
+    player[server.id] = player
+    player.start()
+
+
 
 @bot.command(pass_context = True)
 async def leave(ctx):
     '''
     Leaves the user channel
     '''
-    #server = ctx.message.server
-    voice_client = bot.get_guild(id)
+    server = ctx.message.server
+    voice_client = bot.voice_client_in(server)
     await voice_client.leave()
 
 
