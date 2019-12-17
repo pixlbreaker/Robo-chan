ef9abdccd7d8532a11c6ac2a059229170c2a59fe
Mike Skotar <michaelskotar@gmail.com>
2019-12-16 22:10:41 -0500

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -1,7 +1,12 @@
 import discord
+import youtube_dl
 from discord.ext import commands
 from discord import FFmpegPCMAudio
 from discord.utils import get
+
+
+players = {}
+
 
 class Music(commands.Cog):
     def __init__(self, bot):
@@ -13,30 +18,40 @@ class Music(commands.Cog):
     
     @commands.command(pass_context=True)
     async def join(self, ctx):
-        '''Joins the users channel'''
-        channel = ctx.message.author.voice.channel
-        if not channel:
-            await ctx.send("You are not connected to a voice channel")
-            return
-        voice = get(self.bot.voice_clients, guild=ctx.guild)
-        if voice and voice.is_connected():
-            await voice.move_to(channel)
-            await ctx.send("Moved channel")
-        else:
-            voice = await channel.connect()
-            await ctx.send("Joined channel")
-        # source = FFmpegPCMAudio('1.m4a')
-        # player = voice.play(source)
+        """Joins the users channel"""
+        try:
+            channel = ctx.message.author.voice.channel
+            voice = get(self.bot.voice_clients, guild=ctx.guild)
+            if voice and voice.is_connected():
+                await voice.move_to(channel)
+                await ctx.send("Moved channel")
+            else:
+                voice = await channel.connect()
+                await ctx.send("Joined channel")
+        except:
+            await ctx.send("You are not connected to a channel")
+        
     
     @commands.command()
     async def leave(self, ctx):
-        server = ctx.message.server
-        voice_client = client.voice_client_in(server)
-        if voice_client:
-            await voice_client.disconnect()
-            print("Bot left the voice channel")
+        """Leaves the user channel"""
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+        if voice and voice.is_connected():
+            await ctx.send("Disconnecting from channel")
+            await voice.disconnect()
         else:
-            print("Bot was not in channel")
+            await ctx.send("")
+    
+    @commands.command()
+    async def play(self, ctx, url: str):
+
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+        voice.play(url)
+        # guild = ctx.message.guild
+        # voice_client = guild.voice_client
+        # player = await voice_client.create_ytdl_player(url)
+        # players[server.id] = player
+        # player.start()
 
 def setup(bot):
     bot.add_cog(Music(bot))
